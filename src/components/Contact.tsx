b5ef24dcfea77f6929fda7951ce89c8c60512954
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-13 17:20:56 -0300

    Update contact section padding and icons

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,12 @@
 const Contact = () => {
     return (
         <section className="contact mx-4" id="contact">
-            <h2 className="display-3 text-center mb-4">Contact me</h2>
+            <h2 className="display-3 text-center mb-5">Contact me</h2>
 
-            <div className="contact-container mx-auto d-flex flex-column flex-md-row justify-content-center rounded shadow-lg gap-4 p-4">
+            <div className="contact-container mx-auto d-flex flex-column justify-content-center align-items-start rounded shadow-lg gap-4 p-5">
                 <div className="d-flex justify-content-center align-items-center gap-2">
                     <div className="justify-content-center">
-                        <img src="./public/email.png" alt="Email logo" className="email-icon cursor rounded-circle shadow-lg"/>
+                        <img src="./public/email.png" alt="Email logo" className="logo cursor rounded-circle"/>
                     </div>
                     <div>
                         <p className="mb-0">jjerezbaraona@gmail.com</p>
@@ -15,10 +15,28 @@ const Contact = () => {
 
                 <div className="d-flex justify-content-center align-items-center gap-2">
                     <div className="justify-content-center">
-                        <img src="./public/linkedin.png" alt="LinkedIn logo" className="logo cursor rounded-circle shadow-lg"/>
+                        <img src="./public/linkedin.png" alt="LinkedIn logo" className="logo cursor rounded-circle"/>
                     </div>
                     <div>
                         <p className="mb-0">LinkedIn</p>
+                    </div>
+                </div>
+
+                <div className="d-flex justify-content-center align-items-center gap-2">
+                    <div className="justify-content-center">
+                        <img src="./public/whatsapp.svg" alt="WhatsApp logo" className="logo cursor"/>
+                    </div>
+                    <div>
+                        <p className="mb-0">+569 8360 1614</p>
+                    </div>
+                </div>
+
+                <div className="d-flex justify-content-center align-items-center gap-2">
+                    <div className="justify-content-center">
+                        <img src="./public/gps.svg" alt="GPS logo" className="logo cursor rounded-circle"/>
+                    </div>
+                    <div>
+                        <p className="mb-0">Santiago, Chile</p>
                     </div>
                 </div>
             </div>
