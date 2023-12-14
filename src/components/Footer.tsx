ed3a601cc542adb1bd9c8d48aba76ffc78c48177
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-14 08:05:29 -0300

    Update footer and header styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <a className="text-white underlined fs-5" href="#projects">Projects</a>
             <a className="text-white underlined fs-5" href="#skills">Skills</a>
         </nav>
-        <p className="mb-3 text-white text-center">Copyright &#169; 2023 Juan Manuel Jerez Baraona. Todos los derechos reservados.</p>
+        <p className="mb-3 text-white text-center">Copyright &#169; 2023 Juan Manuel Jerez Baraona. All rights reserved.</p>
     </footer>
   )
 }
