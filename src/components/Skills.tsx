9777cf7f3b32cfbb684e8f955a240427e3335552
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-17 17:33:25 -0300

    Update skill names to use paragraphs instead of headings

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -14,91 +14,91 @@ const Skills: React.FC<SkillsProps> = ({ darkMode }) => {
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/html.svg" alt="Icon of HTML" />
-                            <h5 className="mb-0">HTML</h5>
+                            <p className="mb-0 fs-5 fw-bold">HTML</p>
                         </div>
                     </article>
 
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/css.svg" alt="Icon of CSS" />
-                            <h5 className="mb-0">CSS</h5>
+                            <p className="mb-0 fs-5 fw-bold">CSS</p>
                         </div>
                     </article>
 
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/sass.svg" alt="Icon of SASS" />
-                            <h5 className="mb-0">SASS</h5>
+                            <p className="mb-0 fs-5 fw-bold">SASS</p>
                         </div>
                     </article>
 
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/bootstrap.svg" alt="Icon of Bootstrap" />
-                            <h5 className="mb-0">Bootstrap</h5>
+                            <p className="mb-0 fs-5 fw-bold">Bootstrap</p>
                         </div>
                     </article>
 
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/tailwind.svg" alt="Icon of Tailwind" />
-                            <h5 className="mb-0">Tailwind</h5>
+                            <p className="mb-0 fs-5 fw-bold">Tailwind</p>
                         </div>
                     </article>
 
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/javascript.svg" alt="Icon of JavaScript" />
-                            <h5 className="mb-0">JavaScript</h5>
+                            <p className="mb-0 fs-5 fw-bold">JavaScript</p>
                         </div>
                     </article>
 
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/typescript.svg" alt="Icon of TypeScript" />
-                            <h5 className="mb-0">TypeScript</h5>
+                            <p className="mb-0 fs-5 fw-bold">TypeScript</p>
                         </div>
                     </article>
 
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/react.svg" alt="Icon of React" />
-                            <h5 className="mb-0">React</h5>
+                            <p className="mb-0 fs-5 fw-bold">React</p>
                         </div>
                     </article>
 
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/react.svg" alt="Icon of React Native" />
-                            <h5 className="mb-0">React Native</h5>
+                            <p className="mb-0 fs-5 fw-bold">React Native</p>
                         </div>
                     </article>
 
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/git.svg" alt="Icon of Git" />
-                            <h5 className="mb-0">Git</h5>
+                            <p className="mb-0 fs-5 fw-bold">Git</p>
                         </div>
                     </article>
 
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/postgres.svg" alt="Icon of PostgreSQL" />
-                            <h5 className="mb-0">PostgreSQL</h5>
+                            <p className="mb-0 fs-5 fw-bold">PostgreSQL</p>
                         </div>
                     </article>
 
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/node.svg" alt="Icon of Node" />
-                            <h5 className="mb-0">Node</h5>
+                            <p className="mb-0 fs-5 fw-bold">Node</p>
                         </div>
                     </article>
 
                     <article>
                         <div className="d-flex align-items-center gap-2 icon-box">
                             <img className="icon" src="/figma.svg" alt="Icon of Figma" />
-                            <h5 className="mb-0">Figma</h5>
+                            <p className="mb-0 fs-5 fw-bold">Figma</p>
                         </div>
                     </article>
 
