21bcb41443f8a6c608e934540b62ca3b87dadc00
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-21 16:25:52 -0300

    Update Header component styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,14 +19,14 @@ const Header: React.FC<HeaderProps> = ({darkMode}) => {
     };
 
     return (
-        <header className="container-fluid header background-header">
-            <div className="row align-items-center">
+        <header className="container-fluid header background-header px-lg-5">
+            <div className="row align-items-center mx-lg-5 px-lg-5">
 
-                <div className="col-md-5 d-flex justify-content-center mb-3">
+                <div className="col-md-5 col-lg-6 d-flex justify-content-center mb-3">
                     <img className="img-fluid perfil rounded-circle shadow-lg" src="/foto2-perfil.png" alt="Profile picture" />
                 </div>
 
-                <div className="col-md-7 d-flex flex-column align-items-center">
+                <div className="col-md-7 col-lg-6 d-flex flex-column align-items-center">
                     <p className="mb-1 fs-5">Hello! I'm</p>
                     <h1 className="display-3">Juan Manuel Jerez</h1>
                     <p className={`text-center fs-2 mb-2 ${darkMode ? 'bg-dark rounded px-3' : 'bg-success rounded px-3'}`}>Fullstack Developer</p>
