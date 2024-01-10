351f008d43b6a84d78c78773994931b84a46bff5
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-10 09:19:47 -0300

    Refactor Footer component layout and add contact information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,9 +9,25 @@ const Footer = () => {
                     <div className="w-75 mx-auto"><a className="text-primary underlined fs-5" href="#skills"><i className="bi bi-floppy"></i> Skills</a></div>
                 </nav>
                 <nav className="col-12 col-md-6 d-flex flex-column border-bottom pb-4 gap-2 mb-4 my-lg-3">
-                    <div className="w-75 mx-auto text-primary fs-5"><i className="bi bi-envelope-at"></i> jjerezbaraona@gmail.com</div>
-                    <div className="w-75 mx-auto"><a className="text-primary underlined fs-5" href="https://wa.me/56983601614" target="_blank"><i className="bi bi-whatsapp"></i> +569 8360 1614</a></div>
-                    <div className="w-75 mx-auto text-primary fs-5"><i className="bi bi-geo-alt"></i> Santiago, Chile</div>
+
+                    <div className="w-75 mx-auto text-primary fs-5">
+                        <i className="bi bi-envelope-at"></i> jjerezbaraona@gmail.com
+                    </div> 
+
+                    <div className="w-75 mx-auto">
+                        <a 
+                            className="text-primary underlined fs-5" 
+                            href="https://wa.me/56983601614" 
+                            target="_blank"
+                        >
+                            <i className="bi bi-whatsapp"></i> +569 8360 1614
+                        </a>
+                    </div>
+                    
+                    <div className="w-75 mx-auto text-primary fs-5">
+                        <i className="bi bi-geo-alt"></i> Santiago, Chile
+                    </div>
+
                     <div className="w-75 mx-auto">
                         <a 
                             className="text-primary underlined fs-5" 
@@ -22,6 +38,7 @@ const Footer = () => {
                             <i className="bi bi-linkedin"></i> LinkedIn
                         </a>
                     </div>
+
                     <div className="w-75 mx-auto">
                         <a 
                             className="text-primary underlined fs-5" 
@@ -32,9 +49,10 @@ const Footer = () => {
                             <i className="bi bi-github"></i> GitHub
                         </a>
                     </div>
+
                 </nav>
             </div>
-            <p className="my-2 text-primary text-center">Copyright &#169; 2024 Juan Manuel Jerez Baraona. All rights reserved.</p>
+            <p className="my-2 text-primary text-center">Copyright &#169; 2024. Juan Manuel Jerez Baraona. All rights reserved.</p>
         </footer>
     )
 }
