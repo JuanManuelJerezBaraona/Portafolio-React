a04081d7f7a2b20e83b9ea429d1e43717a8786c1
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-10 08:56:53 -0300

    Refactor footer component layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,21 @@
 const Footer = () => {
   return (
     <footer className="bg-dark p-3">
-        <nav className="container d-flex justify-content-center border-bottom pb-4 gap-5 my-4">
-            <a className="text-primary underlined fs-4" href="#home">Home</a>
-            <a className="text-primary underlined fs-4" href="#projects">Projects</a>
-            <a className="text-primary underlined fs-4" href="#skills">Skills</a>
-        </nav>
-        <p className="mb-3 text-primary text-center">Copyright &#169; 2024 Juan Manuel Jerez Baraona. All rights reserved.</p>
+        <div className="row">
+            <nav className="col-12 col-md-6 d-flex flex-column border-bottom pb-4 gap-2 mb-4 my-lg-3">
+                <div className="w-75 mx-auto"><a className="text-primary underlined fs-5" href="#home"><i className="bi bi-house"></i> Home</a></div>
+                <div className="w-75 mx-auto"><a className="text-primary underlined fs-5" href="#projects"><i className="bi bi-person-workspace"></i> Projects</a></div>
+                <div className="w-75 mx-auto"><a className="text-primary underlined fs-5" href="#skills"><i className="bi bi-floppy"></i> Skills</a></div>
+            </nav>
+            <nav className="col-12 col-md-6 d-flex flex-column border-bottom pb-4 gap-2 mb-4 my-lg-3">
+                <div className="w-75 mx-auto"><a className="text-primary underlined fs-5" href="#home"><i className="bi bi-envelope-at"></i> jjerezbaraona@gmail.com</a></div>
+                <div className="w-75 mx-auto"><a className="text-primary underlined fs-5" href="#projects"><i className="bi bi-whatsapp"></i> +569 8360 1614</a></div>
+                <div className="w-75 mx-auto"><a className="text-primary underlined fs-5" href="#projects"><i className="bi bi-geo-alt"></i> Santiago, Chile</a></div>
+                <div className="w-75 mx-auto"><a className="text-primary underlined fs-5" href="#skills"><i className="bi bi-linkedin"></i> LinkedIn</a></div>
+                <div className="w-75 mx-auto"><a className="text-primary underlined fs-5" href="#skills"><i className="bi bi-github"></i> GitHub</a></div>
+            </nav>
+        </div>
+        <p className="my-2 text-primary text-center">Copyright &#169; 2024 Juan Manuel Jerez Baraona. All rights reserved.</p>
     </footer>
   )
 }
