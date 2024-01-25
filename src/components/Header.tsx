a406e77d48cdc58a2b81f791a67dc7ebe196ad18
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-25 15:53:01 -0300

    Update color transition duration in underlined class

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ const Header: React.FC<HeaderProps> = ({darkMode}) => {
                         <h1 className={`display-2 mb-3 ${darkMode ? 'bg-dark rounded-pill px-5 py-1' : 'bg-success rounded-pill px-5 py-1'}`}>Juan Jerez</h1>
                         {/* <p className={`text-center fs-3 mb-2 ${darkMode ? 'bg-dark rounded-pill px-3 py-1' : 'bg-success rounded-pill px-3 py-1'}`}>Fullstack Developer</p> */}
                         <Button 
-                            className={`btn ${darkMode ? 'btn-dark border-primary' : 'btn-light'} btn-lg rounded-pill shadow-lg fw-bold`}
+                            className={`btn ${darkMode ? 'btn-dark border-primary' : 'btn-light border-dark'} btn-lg rounded-pill shadow-lg fw-bold`}
                             onClick={downloadCV}
                         >
                         Download CV
