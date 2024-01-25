5061be8969fcc1c69b4fd2928a811288bb8f5267
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-25 15:20:30 -0300

    Update About component layout and content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,18 +1,18 @@
 const About = () => {
-  return (
-    <>
-        <section className="about container-fluid about bg-dark pb-2 pb-lg-5 mb-4 mb-lg-5 border-bottom">
-            <div className="row align-items-center pb-5">
+    return (
+        <>
+            <section className="about container-xl about pb-2 pb-lg-5 mb-4 mb-lg-5 border-bottom">
+                <div className="row align-items-center pb-5">
 
-                <div className="col-md-12">
-                    <h2 className="display-5 text-primary text-center mb-4">About me</h2>
-                    <p className="text-primary w-75 mx-auto fs-5">Passionate about <strong>Programming</strong> and <strong>Science</strong>, I stand out in <strong>Full Stack Web Development</strong>, always looking for new challenges and learning opportunities.<br /><br />With a disciplined focus, I balance my professional development with personal interests such as <strong>reading</strong>, <strong>weightlifting</strong>, and <strong>running</strong>. These activities not only keep me physically and mentally in shape, but also enhance my focus and perseverance, essential qualities in my professional journey.</p>
-                </div> 
+                    <div className="col-md-12">
+                        <h2 className="display-5 text-center mb-4">About me</h2>
+                        <p className="w-75 mx-auto fs-5">Passionate about <strong>Programming</strong> and <strong>Science</strong>, I stand out in <strong>Full Stack Web Development</strong>, always looking for new challenges and learning opportunities. <br /><br />With a disciplined focus, I balance my professional development with personal interests such as <strong>reading</strong>, <strong>weightlifting</strong>, and <strong>running</strong>. <br /><br />These activities not only keep me physically and mentally in shape, but also enhance my focus and perseverance, essential qualities in my professional journey.</p>
+                    </div> 
 
-            </div>
-        </section>
-    </>
-  )
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default About
