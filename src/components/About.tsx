0d75fa02fd305cf43759c0945dec052634e823a6
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-25 15:29:47 -0300

    Update About component with emojis

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
 
                     <div className="col-md-12">
                         <h2 className="display-5 text-center mb-4">About me</h2>
-                        <p className="w-75 mx-auto fs-5">Passionate about <strong>Programming</strong> and <strong>Science</strong>, I stand out in <strong>Full Stack Web Development</strong>, always looking for new challenges and learning opportunities. <br /><br />With a disciplined focus, I balance my professional development with personal interests such as <strong>reading</strong>, <strong>weightlifting</strong>, and <strong>running</strong>. <br /><br />These activities not only keep me physically and mentally in shape, but also enhance my focus and perseverance, essential qualities in my professional journey.</p>
+                        <p className="w-75 mx-auto fs-5">Passionate about <strong>Programming</strong> and <strong>Science</strong>, I stand out in <strong>Full Stack Web Development</strong>, always looking for new challenges and learning opportunities. 💻🔬<br /><br />With a disciplined focus, I balance my professional development with personal interests such as <strong>reading</strong>, <strong>weightlifting</strong>, and <strong>running</strong>. 📖🏋️‍♂️<br /><br />These activities not only keep me physically and mentally in shape, but also enhance my focus and perseverance, essential qualities in my professional journey. 💪🧠</p>
                     </div> 
 
                 </div>
