ac54fc94d1ebaa0b29f62de5711040122422aa17
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-03-08 13:54:01 -0300

    Update NavigationBar styling and layout

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -10,19 +10,19 @@ const NavigationBar: React.FC<NavigationBarProps> = ({ darkMode, toggleDarkMode 
     return (
         <Navbar expand="lg" className={`${darkMode ? 'bg-dark' : 'bg-success'}`} id="home">
             <Container>
-                <Navbar.Brand href="" className='fs-4 fw-bold'>Juan Jerez</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Brand href="" className='mx-auto fs-4 fw-bold'>Juan Jerez</Navbar.Brand>
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                    <Nav.Link href="#projects" className='me-auto underlined fs-5'>Projects</Nav.Link>
-                    <Nav.Link href="#skills" className='me-auto underlined fs-5'>Skills</Nav.Link>
-                    <Nav.Link href="#contact" className='me-auto underlined fs-5'>Contact</Nav.Link>
+                    <Nav.Link href="#projects" className='me-auto px-1 px-lg-3 underlined fs-5'>Projects</Nav.Link>
+                    <Nav.Link href="#skills" className='me-auto px-1 px-lg-3 underlined fs-5'>Skills</Nav.Link>
+                    <Nav.Link href="#contact" className='me-auto px-1 px-lg-3 underlined fs-5'>Contact</Nav.Link>
                 </Nav>
                 <div className='d-flex justify-content-start'>
                 <Nav.Link 
                     onClick={toggleDarkMode}
                     aria-label={darkMode ? 'Switch to light mode' : 'Switch to dark mode'}
-                    className='me-auto px-0 px-lg-3 pt-2 pb-1 underlined fs-5'
+                    className='me-auto px-1 px-lg-3 pt-2 pb-1 underlined fs-5'
                 >
                     <i className={`bi ${darkMode ? 'bi-sun-fill' : 'bi-moon-stars-fill'}`}></i>
                 </Nav.Link>
